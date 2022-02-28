922a059e86c8b315a19dfb02dc2d8c755ccea27c
songph <1844349615@qq.com>
2022-02-28 23:59:20 +0800

    添加项目文件。

---
+++ W1HW3App/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W1HW3App
+{
+    internal class DayPrinter
+    {
+        static void Main(string[] args)
+        {
+            String[] days = { "Enter Year:", "2000", "2000 is a leap Year." };
+            for (int i = 0; i < days.Length; i++)
+            {
+                Console.WriteLine(days[i]);
+            }
+        }
+    }
+}
